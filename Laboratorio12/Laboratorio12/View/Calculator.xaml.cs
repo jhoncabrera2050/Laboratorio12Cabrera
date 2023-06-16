a8353093db05d88e1a761c2da9c2a684bc728a81
Alumno <Alumno@LAB1509-05>
2023-06-16 14:47:33 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio12/Laboratorio12/View/Calculator.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Laboratorio12.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Laboratorio12.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Calculator : ContentPage
+    {
+
+        private CalculatorViewModel viewModel;
+
+        public Calculator()
+        {
+            InitializeComponent();
+            viewModel = new CalculatorViewModel();
+            BindingContext = viewModel;
+        }
+
+        void OnSelectNumber(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            viewModel.OnSelectNumber(pressed);
+        }
+
+        void OnSelectOperator(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            viewModel.OnSelectOperator(pressed);
+        }
+
+        void OnClear(object sender, EventArgs e)
+        {
+            viewModel.OnClear();
+        }
+
+        void OnCalculate(object sender, EventArgs e)
+        {
+            viewModel.OnCalculate();
+        }
+
+    }
+}
---
+++ Laboratorio12/Laboratorio12/ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Laboratorio12.ViewModel
+{
+    public class CalculatorViewModel : ViewModelBase
+    {
+
+        private string resultText;
+        public string ResultText
+        {
+            get { return resultText; }
+            set
+            {
+                if (resultText != value)
+                {
+                    resultText = value;
+                    OnPropertyChanged(nameof(ResultText));
+                }
+            }
+        }
+
+        private double firstNumber;
+        private double secondNumber;
+        private string mathOperator;
+        private int currentState;
+
+        public CalculatorViewModel()
+        {
+            OnClear();
+        }
+
+        public void OnSelectNumber(string pressed)
+        {
+            if (ResultText == "0" || currentState < 0)
+            {
+                ResultText = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            ResultText += pressed;
+
+            double number;
+            if (double.TryParse(ResultText, out number))
+            {
+                ResultText = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber = number;
+                }
+                else
+                {
+                    secondNumber = number;
+                }
+            }
+        }
+
+        public void OnSelectOperator(string pressed)
+        {
+            currentState = -2;
+            mathOperator = pressed;
+        }
+
+        public void OnClear()
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            ResultText = "0";
+        }
+
+        public void OnCalculate()
+        {
+            if (currentState == 2)
+            {
+                var result = SimpleCalculator.Calculate(firstNumber, secondNumber, mathOperator);
+
+                ResultText = result.ToString();
+                firstNumber = result;
+                currentState = -1;
+            }
+        }
+
+    }
+}
